f80cc6736bc10c27056479587991e8c44bdd8798
dashattit <dashyla.2021@mail.ru>
2024-11-05 12:33:49 +0700

    create models

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
 # Register your models here.
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Author, AuthorAdmin)
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+import uuid #Требуется для уникальных экземпляров книг
 
 # Create your models here.
 # определение модели
@@ -44,7 +45,7 @@ class Book(models.Model):
     #  Author в виде строки, а не объекта, поскольку он еще не был объявлен в файле.
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    genre = models.ManyToManyField('Genre', help_text="Select a genre for this book")
     # Поле ManyToManyField используется потому, что жанр может содержать много книг. Книги могут охватывать множество жанров.
     # Класс жанра уже определен, поэтому мы можем указать объект, указанный выше.
 
@@ -56,6 +57,38 @@ class Book(models.Model):
     def get_absolute_url(self):
         # Возвращает URL-адрес для доступа к определенному экземпляру книги.
         return reverse('book-detail', args=[str(self.id)])
+
+
+
+# Модель BookInstance
+class BookInstance(models.Model):
+    """
+    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
+    class Meta:
+        ordering = ["due_back"]
+
+
+    def __str__(self):
+        """
+        String for representing the Model object
+        """
+        return '%s (%s)' % (self.id,self.book.title)
+
 
 
 # модель автора
